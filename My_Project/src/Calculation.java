7da8c9492dba53edf011d95bcfddab0e5aac81b2
shebin-zeo <shebin2024@gmail.com>
2024-02-11 10:09:49 +0530

    File is created

---
+++ My_Project/src/Calculation.java
@@ -0,0 +1,28 @@
+import java.awt.desktop.SystemEventListener;
+
+public class Calculation {
+    int a; //Variable
+    int b; //Variable
+    void sum(int num1,int num2)
+    {
+        int result=num1+num2;
+        System.out.println("The addition of two number is "+result);
+    }
+    void sub(int num1,int num2)
+    {
+        int result=num1-num2;
+        System.out.println("The subtraction of two number is "+result);
+    }
+    void mul(int num1,int num2)
+    {
+        int result=num1*num2;
+        System.out.println("The multiplication of two number is "+result);
+    }
+    void division(int num1,int num2)
+    {
+        int result=num1/num2;
+        System.out.println("The division of two number "+result);
+
+
+    }
+}
